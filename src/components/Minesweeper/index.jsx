bfb6e060b9d5c8016f41e55d60dc0ca598d2c022
Adam Magomadov <adamshumag@gmail.com>
2022-05-19 22:20:58 +0300

    fix bomb at first cell

--- src/components/Minesweeper/index.jsx
+++ src/components/Minesweeper/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Cell from '../Cell';
 import Row from '../Row';
 import styles from './styles.scss';
@@ -6,6 +6,24 @@ import styles from './styles.scss';
 function randomInteger(min, max) {
   const rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
+}
+
+function openCells(field, x, y) {
+  const cell = field?.[y]?.[x];
+
+  if (cell?.opened || !cell) return;
+
+  const prevCell = { ...cell, opened: true };
+  field[y][x] = prevCell;
+
+  if (cell.around === 0) {
+    openCells(field, x, y - 1);
+    openCells(field, x - 1, y);
+    openCells(field, x + 1, y);
+    openCells(field, x, y + 1);
+  }
+
+  return field;
 }
 
 function generateField(rows, cols) {
@@ -21,62 +39,64 @@ function generateField(rows, cols) {
         }));
     });
 
-  let bombsCount = (rows + cols) / 2;
-
-  while (bombsCount) {
-    const randomRow = randomInteger(0, rows - 1);
-    const randomCol = randomInteger(0, cols - 1);
-
-    if (field[randomRow][randomCol].isBomb) {
-      continue;
-    }
-
-    field[randomRow][randomCol].isBomb = true;
-
-    for (let i = -1; i <= 1; i++) {
-      for (let j = -1; j <= 1; j++) {
-        field[randomRow + i]?.[randomCol + j] && field[randomRow + i][randomCol + j].around++;
-      }
-    }
-
-    bombsCount--;
-  }
-
   return field;
 }
 
 function Minesweeper() {
-  const [field, setField] = useState(generateField(10, 12));
+  const [size, setSize] = useState([10, 10]);
+  const [field, setField] = useState(generateField(size[0], size[1]));
+  const isFirstClick = useRef(true);
 
   // console.log(field);
 
   function handleClick(evt, x, y, isBomb) {
+    if (isFirstClick.current) {
+      generateBombs(x, y);
+    }
+
+    isFirstClick.current = false;
+
     if (isBomb) {
       alert('You lose');
       return;
     }
 
-    const prevField = openCells([...field], x, y);
-
-    setField(prevField);
+    setField(prevField => openCells([...prevField], x, y));
   }
 
-  function openCells(field, x, y) {
-    const cell = field?.[y]?.[x];
+  function generateBombs(x, y) {
+    let bombsCount = (size[0] + size[1]) / 2;
 
-    if (cell?.opened || !cell) return;
+    setField(prevField => {
+      const field = [...prevField];
 
-    const prevCell = { ...cell, opened: true };
-    field[y][x] = prevCell;
+      while (bombsCount) {
+        const randomRow = randomInteger(0, size[0] - 1);
+        const randomCol = randomInteger(0, size[1] - 1);
 
-    if (cell.around === 0) {
-      openCells(field, x, y - 1);
-      openCells(field, x - 1, y);
-      openCells(field, x + 1, y);
-      openCells(field, x, y + 1);
-    }
+        if (field[randomRow][randomCol].isBomb || (randomRow === y && randomCol === x)) {
+          continue;
+        }
 
-    return field;
+        const prevCell = { ...field[randomRow][randomCol] };
+        prevCell.isBomb = true;
+        field[randomRow][randomCol] = prevCell;
+
+        for (let i = -1; i <= 1; i++) {
+          for (let j = -1; j <= 1; j++) {
+            if (field[randomRow + i]?.[randomCol + j]) {
+              const prevCell = { ...field[randomRow + i][randomCol + j] };
+              prevCell.around++;
+              field[randomRow + i][randomCol + j] = prevCell;
+            }
+          }
+        }
+
+        bombsCount--;
+      }
+
+      return field;
+    });
   }
 
   return (
