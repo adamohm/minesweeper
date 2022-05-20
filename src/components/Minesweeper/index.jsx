e0ffc8685891e078e9f4444cca350df925c48f6b
Adam Magomadov <adamshumag@gmail.com>
2022-05-20 06:10:28 +0300

    add ability to change field size

--- src/components/Minesweeper/index.jsx
+++ src/components/Minesweeper/index.jsx
@@ -13,8 +13,8 @@ function openCells(field, x, y) {
 
   if (cell?.status === 1 || !cell) return;
 
-  const prevCell = { ...cell, status: 1 };
-  field[y][x] = prevCell;
+  const newCell = { ...cell, status: 1 };
+  field[y][x] = newCell;
 
   if (cell.around === 0) {
     openCells(field, x, y - 1);
@@ -51,8 +51,8 @@ function Minesweeper() {
   const timerId = useRef(null);
 
   useEffect(() => {
-    const bombsCount = (size[0] + size[1]) / 2;
-    const closedCells = field.flat().filter(cell => cell.status === 0);
+    const bombsCount = ~~(((size[0] + size[1]) / 2) * 1.5);
+    const closedCells = field.flat().filter(cell => cell.status === 0 || cell.status === 2);
 
     if (bombsCount === closedCells.length) {
       setIsGameOver('You won!');
@@ -86,38 +86,38 @@ function Minesweeper() {
     if (isGameOver) return;
 
     setField(prevField => {
-      const field = [...prevField];
-      const prevCell = { ...prevField[y][x], status: prevField[y][x].status === 2 ? 0 : 2 };
-      field[y][x] = prevCell;
+      const newField = [...prevField];
+      const newCell = { ...newField[y][x], status: newField[y][x].status === 2 ? 0 : 2 };
+      newField[y][x] = newCell;
 
-      return field;
+      return newField;
     });
   }
 
   function generateBombs(x, y) {
-    let bombsCount = (size[0] + size[1]) / 2;
+    let bombsCount = ~~(((size[0] + size[1]) / 2) * 1.5);
 
     setField(prevField => {
-      const field = [...prevField];
+      const newField = [...prevField];
 
       while (bombsCount) {
         const randomRow = randomInt(0, size[0] - 1);
         const randomCol = randomInt(0, size[1] - 1);
 
-        if (field[randomRow][randomCol].isBomb || (randomRow === y && randomCol === x)) {
+        if (newField[randomRow][randomCol].isBomb || (randomRow === y && randomCol === x)) {
           continue;
         }
 
-        const prevCell = { ...field[randomRow][randomCol] };
-        prevCell.isBomb = true;
-        field[randomRow][randomCol] = prevCell;
+        const newCell = { ...newField[randomRow][randomCol] };
+        newCell.isBomb = true;
+        newField[randomRow][randomCol] = newCell;
 
         for (let i = -1; i <= 1; i++) {
           for (let j = -1; j <= 1; j++) {
-            if (field[randomRow + i]?.[randomCol + j]) {
-              const prevCell = { ...field[randomRow + i][randomCol + j] };
-              prevCell.around++;
-              field[randomRow + i][randomCol + j] = prevCell;
+            if (newField[randomRow + i]?.[randomCol + j]) {
+              const newCell = { ...newField[randomRow + i][randomCol + j] };
+              newCell.around++;
+              newField[randomRow + i][randomCol + j] = newCell;
             }
           }
         }
@@ -125,11 +125,26 @@ function Minesweeper() {
         bombsCount--;
       }
 
-      return field;
+      return newField;
     });
   }
 
   function restart() {
+    if (size[0] < 10 || size[1] < 10) {
+      alert('10x10 is minimal size for the field!');
+      return;
+    }
+
+    if (size[0] > 18) {
+      alert('18 is maximum size for the rows!');
+      return;
+    }
+
+    if (size[1] > 25) {
+      alert('20 is maximum size for the columns!');
+      return;
+    }
+
     setField(generateField(size[0], size[1]));
     setIsGameOver(false);
     clearInterval(timerId.current);
@@ -138,12 +153,25 @@ function Minesweeper() {
     timerId.current = null;
   }
 
+  function handleChange(evt) {
+    const value = +evt.target.value;
+    const name = evt.target.name;
+
+    if (name === 'rows') {
+      setSize(st => [value, st[1]]);
+    } else {
+      setSize(st => [st[0], value]);
+    }
+  }
+
   return (
     <div>
       <div className={styles.panel}>
         <div className={styles.timer}>⌛ {timer}</div>
         <div className={styles.message}>{isGameOver}</div>
         <div className={styles.controls}>
+          <input onChange={handleChange} name='rows' value={size[0]} className={styles.input} />x
+          <input onChange={handleChange} name='cols' value={size[1]} className={styles.input} />
           <button onClick={restart} className={styles.btn}>
             Restart
           </button>
